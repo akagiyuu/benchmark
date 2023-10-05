dcdfb4cab2742fc7be98e6a8124de73e6344a0a1
AkagiYuu <khanghuynh0245@gmail.com>
2023-10-05 15:19:39 +0700

    feat(simd bench): add rayon

--- simd/src/lib.rs
+++ simd/src/lib.rs
@@ -2,6 +2,7 @@
 #![feature(portable_simd)]
 #![feature(test)]
 
+use rayon::prelude::*;
 use std::simd::{f32x4, SimdFloat};
 
 pub fn dot_product(a: &[f32], b: &[f32]) -> f32 {
@@ -15,6 +16,10 @@ pub fn dot_product_simd(a: &[f32], b: &[f32]) -> f32 {
         .zip(b.array_chunks::<4>().map(|&chunk| f32x4::from_array(chunk)))
         .fold(f32x4::splat(0.), |acc, (a, b)| acc + a * b)
         .reduce_sum()
+}
+
+pub fn dot_product_rayon(a: &[f32], b: &[f32]) -> f32 {
+    a.par_iter().zip(b.par_iter()).map(|(a, b)| a * b).sum()
 }
 
 #[cfg(test)]
@@ -56,9 +61,19 @@ mod tests {
         let b = [6., 1., 5., 7., 4., 3., 2., 8.];
         assert_eq!(dot_product_simd(&a, &b), 211.);
     }
+    #[test]
+    fn dot_product_rayon_test() {
+        let a = [1., 2., 3., 4.];
+        let b = [5., 6., 7., 8.];
+        assert_eq!(dot_product_rayon(&a, &b), 70.);
+
+        let a = [5., 63., 2., 3., 4., 1., 6., 7.];
+        let b = [6., 1., 5., 7., 4., 3., 2., 8.];
+        assert_eq!(dot_product_rayon(&a, &b), 211.);
+    }
 
     const RANGE: Range<f32> = -100f32..100f32;
-    const SIZE: usize = 1000000;
+    const SIZE: usize = 10000000;
 
     #[bench]
     fn dot_product_bench(bencher: &mut Bencher) {
@@ -75,6 +90,15 @@ mod tests {
             let a = black_box(random_f32_vector(SIZE, RANGE));
             let b = black_box(random_f32_vector(SIZE, RANGE));
             println!("{}", dot_product_simd(&a, &b));
+        })
+    }
+
+    #[bench]
+    fn dot_product_rayon_bench(bencher: &mut Bencher) {
+        bencher.iter(|| {
+            let a = black_box(random_f32_vector(SIZE, RANGE));
+            let b = black_box(random_f32_vector(SIZE, RANGE));
+            println!("{}", dot_product_rayon(&a, &b));
         })
     }
 }
